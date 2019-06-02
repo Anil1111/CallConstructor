f7a5546096f6859c1f1b6e4f3febc731e389d5f5
anilsri.04@Gmail.com <anilsri.04@Gmail.com>
2019-06-02 09:52:41 +0530

    CallConstructor

--- CallingConstructor/Program.cs
+++ CallingConstructor/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace CallingConstructor
+public class Person
 {
-    class Program
+    // Constructor that takes no arguments:
+    public Person()
     {
-        static void Main(string[] args)
-        {
-        }
+        Name = "unknown";
+    }
+    // Constructor that takes one argument:
+    public Person(string name)
+    {
+        Name = name;
+    }
+    // Auto-implemented readonly property:
+    public string Name { get; }
+    // Method that overrides the base class (System.Object) implementation.
+    public override string ToString()
+    {
+        return Name  +" This is verriding of toString() Method";
     }
 }
+class TestPerson
+{
+    static void Main()
+    {
+        // Call the constructor that has no parameters.
+        var person1 = new Person();
+        Console.WriteLine(person1.Name);
+        // Call the constructor that has one parameter.
+        var person2 = new Person("Sarah Jones");
+        Console.WriteLine(person2.Name);
+        // Get the string representation of the person2 instance.
+        Console.WriteLine(person2);
+        Console.WriteLine("Press any key to exit.");
+
+    }
+}
+// Output:
+// unknown
+// Sarah Jones
+// Sarah Jones
+//C# Language S
